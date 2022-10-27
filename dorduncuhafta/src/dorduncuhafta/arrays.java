0d0b3313dfc21d20c7d3cf1bd5cfc702a79a2938
aidata <aidata@10.121.3.188>
2022-10-27 10:38:11 +0300

    dizilerde ters cevirme

--- dorduncuhafta/src/dorduncuhafta/arrays.java
+++ dorduncuhafta/src/dorduncuhafta/arrays.java
@@ -1,5 +1,16 @@
 package dorduncuhafta;
 
 public class arrays {
-
+public static void main(String[]args) {
+	int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+		System.out.printf("%s%8%s%n","Index","Value");
+	for (int counter = 0; counter < array.length; counter++) {
+		
+		System.out.printf("%5d%8d%n",counter,array[counter]);
+	}
+System.out.println("Ters cevirilen dizi:");
+	for(int i=array.length-1 ; i>=0; i--)
+		System.out.print(array[i] + " ");
+	
+}
 }
